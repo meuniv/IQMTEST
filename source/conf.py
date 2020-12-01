fcec785f3ad3eae4af9f7b69bf1f165a6cfb3a60
Vincent Meunier <70446244+meuniv@users.noreply.github.com>
2020-12-01 07:32:21 -0500

    Update conf.py

--- source/conf.py
+++ source/conf.py
@@ -14,8 +14,6 @@
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
 
-import os
-import edx_theme
 # -- Project information -----------------------------------------------------
 
 project = 'IQM Spring 2020'
@@ -41,9 +39,13 @@ exclude_patterns = []
 # a list of builtin themes.
 #
 #html_theme = 'sphinx_rtd_theme'
+import edx_theme
+import os
+
+extensions = []
 html_theme = 'edx-theme'
-html_theme_path = [edx_theme.get_html_theme_path()]
-html_favicon = os.path.join(html_theme_path[0], 'edx_theme', 'static', 'css', 'favicon.ico')
+#html_theme_path = [edx_theme.get_html_theme_path()]
+#html_favicon = os.path.join(html_theme_path[0], 'edx_theme', 'static', 'css', 'favicon.ico')
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
@@ -57,6 +59,8 @@ html_static_path = ['_static']
 extensions = [
     'sphinx.ext.imgmath',
     'sphinxcontrib.yt',
+    'sphinx.ext.todo',
+    'sphinx.ext.githubpages',
     'edx_theme'
 ]
 
@@ -65,6 +69,18 @@ latex_elements = {
 \usepackage{physics}
 ''',
 }
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+#
+#html_theme = 'sphinx_rtd_theme'
+import edx_theme
+import os
+
+extensions = []
+html_theme = 'edx-theme'
+#html_theme_path = [edx_theme.get_html_theme_path()]
+#html_favicon = os.path.join(html_theme_path[0], 'edx_theme', 'static', 'css', 'favicon.ico')
 latex_show_urls = 'footnote'
 
 imgmath_add_tooltips = True
